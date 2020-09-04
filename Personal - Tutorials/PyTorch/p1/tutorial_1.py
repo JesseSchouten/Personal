be2145db307f7dc2fef8a6366a17d6bae9d6de6d
JesseSchouten <jesse.schouten@outlook.com>
2020-09-04 15:08:17 +0200

    Pytorch tutorial 1

--- Personal - Tutorials/PyTorch/p1/tutorial_1.py
+++ Personal - Tutorials/PyTorch/p1/tutorial_1.py
@@ -1,1 +1,34 @@
-print("hello world, here I am")
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+import torch
+
+x = torch.Tensor([5,3])
+y = torch.Tensor([2,1])
+
+print("OUTPUT CELL 1:")
+print(x*y)
+
+#%%
+x = torch.zeros([2,5])
+print("OUTPUT CELL 2:")
+print(x)
+print('shape of object: {}'.format(x.shape))
+
+#%%
+y = torch.rand([2,5])
+print("OUTPUT CELL 3:")
+print(y)
+
+#%%
+y.view([1,10])
+print("OUTPUT CELL 4:")
+print(y)
+
+
+
+
