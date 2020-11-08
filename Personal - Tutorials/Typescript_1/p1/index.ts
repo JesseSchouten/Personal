7ab4d2f774d6415226d686818d159cd6baabe882
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2020-11-08 16:27:14 +0100

    typescript tutorial added

---
+++ Personal - Tutorials/Typescript_1/p1/index.ts
@@ -0,0 +1,24 @@
+/*
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+*/
+
+let myString:String;
+let myNum:number;
+let myVar: any;
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number];
+
+strArr = ['Hello','World']
+numArr = [1,2,3];
+boolArr = [true,false,true];
+
+strNumTuple = ['Hello',4];
+
+console.log(boolArr);
+
